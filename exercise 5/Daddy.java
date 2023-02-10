482a21111203988bf3633f6d36e878ccb30b50ca
Christian Geams M. Suganat <119908376+ichann13@users.noreply.github.com>
2023-02-10 14:23:51 +0800

    Add files via upload

---
+++ exercise 5/Daddy.java
@@ -0,0 +1,13 @@
+public class Daddy {
+    
+   protected String surname = "Suganat";
+   
+   protected void showsurname(){
+
+    System.out.println("My family name is " + surname);
+   } 
+
+
+   
+
+}
